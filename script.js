d612d4dd538fa2615c1a7f3ab1411a425550431b
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-07-24 22:28:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-function fibonacci(num) {
-// your code here
+let fib = [0,1]
+function fibonacci(num){
+ 
+    for(let i = 2; i < num ; i++){
+        fib[i] = fib[i-1]+fib[i-2];
+    }
+    return fib[num-1];
+  
 }
-
-module.exports = fibonacci;
+module.exports = fibonacci
